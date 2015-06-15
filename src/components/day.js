78cc2f4c6229f3fc25fa12e9128b48746e84f011
Claudio Ortolina <claudio@new-bamboo.co.uk>
2015-06-15 07:43:13 +0100

    Renders events count per day.

--- src/components/day.js
+++ src/components/day.js
@@ -1,19 +1,34 @@
 import React from 'react';
 import moment from 'moment';
 import PropTypes from '../prop-types';
+import State from '../state';
 
 let dayClass = (selected) => {
   return selected ? 'day selected' : 'day'
 }
 
+let eventsCursor = State.select('events');
+
+let getEvents = (day) => {
+  return eventsCursor.get().filter((ev) => {
+    return moment(ev.startsAt).isSame(day, 'day');
+  });
+}
+
 class Day extends React.Component {
   render() {
+    let eventsCount;
     let day = moment(this.props.day).format('D');
     let selected = this.props.selected;
+    let dayEvents = getEvents(this.props.day);
+    if (dayEvents.length > 0) {
+      eventsCount = <span className="events-count">{dayEvents.length}</span>
+    }
 
     return (
       <div className={dayClass(selected)}>
         <span className="date">{day}</span>
+        {eventsCount}
       </div>
     )
   }
--- src/state.js
+++ src/state.js
@@ -11,7 +11,33 @@ let startDate = toBeginningOfMonth(currentDate);
 
 let State = new Baobab({
   startDate: startDate,
-  currentDate: currentDate
+  currentDate: currentDate,
+  events: [
+    {
+      id: 1,
+      name: 'Dentist',
+      startsAt: new Date(2015,5,1,10,30),
+      endsAt:   new Date(2015,5,1,11,30)
+    },
+    {
+      id: 2,
+      name: 'Important meeting',
+      startsAt: new Date(2015,5,8,10,30),
+      endsAt:   new Date(2015,5,8,11,30)
+    },
+    {
+      id: 3,
+      name: 'Someone\'s birthday',
+      startsAt: new Date(2015,5,10,10,30),
+      endsAt:   new Date(2015,5,10,11,30)
+    },
+    {
+      id: 4,
+      name: 'Pub Night',
+      startsAt: new Date(2015,5,10,17,30),
+      endsAt:   new Date(2015,5,10,18,30)
+    },
+  ]
 })
 
 let incOneMonth = (current) => {
