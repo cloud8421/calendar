814b30f794c9702497476d627043d15204771526
Claudio Ortolina <claudio@new-bamboo.co.uk>
2015-06-16 18:47:30 +0100

    Makes form stateful.

--- src/components/workspace.js
+++ src/components/workspace.js
@@ -1,25 +1,33 @@
 import React from 'react';
 
-class Workspace extends React.Component {
-  render() {
+const Workspace = React.createClass({
+  getInitialState: () => {
+    return {
+      text: 'i.e. Book dentist for friday at 9.00am'
+    }
+  },
+  shouldComponentUpdate: function(nextProps, nextState) {
+    return nextState.text !== this.state.text;
+  },
+  render: function() {
     return (
       <section className="workspace">
         <form className="event-form">
           <label htmlFor="name">What do you need to do?</label>
-          <input id="name" type="text" value="Do something awesome" />
-          <label htmlFor="day">What day?</label>
-          <input id="day" type="text" value="Friday" />
-          <label htmlFor="from">From</label>
-          <input id="from" type="text" value="9.00am" />
-          <label htmlFor="to">From</label>
-          <input id="to" type="text" value="10.00am" />
+          <input id="name"
+                 type="text"
+                 value={this.state.text}
+                 onChange={this.handleChange} />
           <p className="preview">
             Do something awesome, Friday, from 9.00am to 10.00am.
           </p>
         </form>
       </section>
     )
+  },
+  handleChange: function(evt) {
+    this.setState({text: evt.target.value});
   }
-}
+});
 
 export default Workspace;
