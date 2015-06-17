32ca8350689375a5e3a4efd7acb1e02245474705
Claudio Ortolina <claudio@new-bamboo.co.uk>
2015-06-17 14:50:22 +0100

    Adds fast click

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
+import attachFastClick from 'fastclick';
 import React from 'react';
 import Router from 'react-router';
 
 import Routes from "./routes.js";
+
+attachFastClick(document.body);
 
 Router.run(Routes, (Handler) => {
   React.render(<Handler />, document.body);
